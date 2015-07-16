d56ffb0770d062245dbdbb2bfc5ad5e51670aaa7
OdAhViig <best@BESTtekiMacBook-Air.local>
2015-07-16 08:17:46 +0800

    Initial Commit

---
+++ TWeather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TWeather
+//
+//  Created by Kenneth on 15-7-16.
+//  Copyright (c) 2015年 Kenneth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TWeather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TWeather
+//
+//  Created by Kenneth on 15-7-16.
+//  Copyright (c) 2015年 Kenneth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
